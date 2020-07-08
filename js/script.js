1599b8fd59bdd43d4524450b76a5eb70f1eb31f0
Adebimpe <realadebimpe@gmail.com>
2020-07-08 14:02:22 +0100

    Modified css, html and js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,4 @@
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
- }
-
-// const darkMody = document.getElementsByTagName(body);
-// const button = document.getElementsByTagName(button);
-
-// button.addEventListener('click', () =>{
-//     darkMody.dataset.theme =  darkMody.dataset.theme == "dark" ? "light": "dark";
-// })
+}
