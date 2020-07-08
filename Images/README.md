fc747faab6cd4c8ed14b516491206eeab71381d3
Adebimpe <realadebimpe@gmail.com>
2020-07-08 12:37:12 +0100

    Added images, html, and css

---
+++ Images/README.md
@@ -0,0 +1,1 @@
+# DevCareerTask-Find-me
---
+++ js/script.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
+
+// const darkMody = document.getElementsByTagName(body);
+// const button = document.getElementsByTagName(button);
+
+// button.addEventListener('click', () =>{
+//     darkMody.dataset.theme =  darkMody.dataset.theme == "dark" ? "light": "dark";
+// })
